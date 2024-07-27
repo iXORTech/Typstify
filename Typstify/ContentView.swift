761ae4fe4658834ed764e6700389700777082478
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-07-27 12:45:47 -0400

    Initial Commit
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

---
+++ Typstify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Typstify
+//
+//  Created by Cubik65536 on 2024-07-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Typstify/TypstifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypstifyApp.swift
+//  Typstify
+//
+//  Created by Cubik65536 on 2024-07-27.
+//
+
+import SwiftUI
+
+@main
+struct TypstifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
