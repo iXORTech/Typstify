d4b7436f8bd79031e59bcd96f9b2894ac3325a88
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2024-07-29 00:20:05 -0400

    feat: Add some other states to editor
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- Typstify/ContentView.swift
+++ Typstify/ContentView.swift
@@ -11,9 +11,13 @@ import CodeEditorView
 import LanguageSupport
 
 struct ContentView: View {
-    @State private var source:   String                    = "My awesome code..."
+    @State private var source:   String                    = ""
     @State private var position: CodeEditor.Position       = CodeEditor.Position()
     @State private var messages: Set<TextLocated<Message>> = Set()
+    
+    @State private var theme:            ColorScheme?      = nil
+    @State private var showMinimap:      Bool              = true
+    @State private var wrapText:         Bool              = true
     
     @FocusState private var editorIsFocused: Bool
     
@@ -22,10 +26,16 @@ struct ContentView: View {
     var body: some View {
         HStack {
             VStack {
-                CodeEditor(text: $source, position: $position, messages: $messages, language: .swift())
-                    .environment(\.codeEditorTheme,
-                                  colorScheme == .dark ? Theme.defaultDark : Theme.defaultLight)
-                    .focused($editorIsFocused)
+                CodeEditor(
+                    text: $source,
+                    position: $position,
+                    messages: $messages,
+                    language: .swift(),
+                    layout: CodeEditor.LayoutConfiguration(showMinimap: showMinimap, wrapText: wrapText)
+                )
+                .environment(\.codeEditorTheme,
+                              colorScheme == .dark ? Theme.defaultDark : Theme.defaultLight)
+                .focused($editorIsFocused)
             }
             .onAppear{ editorIsFocused =  true }
             
