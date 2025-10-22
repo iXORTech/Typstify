cd7fd692a68d4224e996fe564eb2270154f674e7
Qian Qian "Qubik"‎ <qubik65536@qubik65536.top>
2025-10-22 13:18:09 -0400

    refactor: Migrating to STTextView [Part 2, WIP]
    Signed-off-by: Qian Qian "Qubik"‎ <qubik65536@qubik65536.top>

---
+++ Typstify/Typstify/AnnotationDataSource.swift
@@ -0,0 +1,12 @@
+//
+//  AnnotationDataSource.swift
+//  Typstify
+//
+//  Created by Cubik65536 on 2025-07-24.
+//
+
+import STAnnotationsPlugin
+
+class AnnotationDataSource: STAnnotationsDataSource {
+    var textViewAnnotations: [any STLineAnnotation] = []
+}
